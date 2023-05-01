91736e6d505c82bced58d6074c54637dbebf941a
MaazTariq409 <maaztariq409@gmail.com>
2023-05-01 12:41:07 +0500

    Initial Commit
    Setup Database Connectivity

---
+++ Data/DbContext.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Portfolio_API.Models;
+
+namespace Portfolio_API.Data
+{
+    public class porfolioContext : DbContext
+    {
+        public porfolioContext(DbContextOptions<porfolioContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<SignUp> signUp { get; set; }
+    }
+}
---
+++ Models/SignUp.cs
@@ -0,0 +1,11 @@
+﻿namespace Portfolio_API.Models
+{
+    public class SignUp
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
