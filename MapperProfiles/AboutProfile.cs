51402cc16295cc361200ce252b474c971fac48b5
MaazTariq409 <maaztariq409@gmail.com>
2023-05-02 17:10:19 +0500

    Added Get and Post endpoint of About controller

---
+++ MapperProfiles/AboutProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Portfolio_API.DTOs;
+using Portfolio_API.Models;
+
+namespace Portfolio_API.MapperProfiles
+{
+    public class AboutProfile : Profile
+    {
+        public AboutProfile()
+        {
+            CreateMap<About, AboutDto>();
+            CreateMap<AboutDto, About>();
+        }
+    }
+}
---
+++ Repository/Repository Interface/IAbout.cs
@@ -0,0 +1,16 @@
+﻿using Portfolio_API.DTOs;
+using Portfolio_API.Models;
+
+namespace Portfolio_API.Repository.Repository_Interface
+{
+    public interface IAbout
+    {
+        public About GetAbout (int id);
+
+        public void AddAbout (int id, About about);
+
+        public void removeAbout (int id);
+
+        public void updateAbout (int id, AboutDto about);
+    }
+}
